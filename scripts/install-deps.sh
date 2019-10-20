ffab5eeb824d44ff31d50f933c0cfac592a49012
Jérôme Blondon <jerome@blondon.fr>
2019-10-20 13:55:01 +0200

    Improve installation
    - postgres password is 'password'
    - timezone set to Europe/Paris

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,6 +3,8 @@ apt-get update
 apt-get install -y git htop dstat vim curl wget inotify-tools \
     openssl dos2unix ntp libpq-dev nodejs npm postgresql-11
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb && rm -rf erlang-solutions_1.0_all.deb
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
 apt-get update
 apt-get -y install esl-erlang elixir
-yes | mix local.hex
